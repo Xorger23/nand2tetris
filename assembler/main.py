802592b69e446fe7e96db051538ca03945979088
Elijah Yajaman <112749251+Xorger@users.noreply.github.com>
2025-03-25 17:49:44 +1000

    Created symbol table and some psudocode

---
+++ assembler/main.py
@@ -0,0 +1,7 @@
+#Init symbol table
+#First pass to eval L-Commands
+#Second pass to eval C- and A- commands
+#USAGE: python main.py inputfile outputfile
+from symboltable import SymbolTable
+
+symbols = SymbolTable()
